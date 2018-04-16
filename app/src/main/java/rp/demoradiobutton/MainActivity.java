8354f41b2f664f66118f6c20f0303650bbd968cd
16022582 <16022582@myrp.edu.sg>
2018-04-16 22:47:10 +0800

    Initial commit

---
+++ app/src/main/java/rp/demoradiobutton/MainActivity.java
@@ -0,0 +1,37 @@
+package rp.demoradiobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    RadioButton rb;
+    RadioGroup rg;
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View args0) {
+                RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(selectedButtonId);
+                Toast.makeText(getBaseContext(), rb.getText(),
+                        Toast.LENGTH_LONG).show();
+
+            }});
+    }
+}
